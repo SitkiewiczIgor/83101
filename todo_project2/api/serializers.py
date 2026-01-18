bfb7c70bd124ab7ab8d97828d48edb9bbeb302dc
drobileuu <79520539+SitkiewiczIgor@users.noreply.github.com>
2026-01-18 16:14:59 +0100

    Delete todo_project2 directory

--- todo_project2/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Task
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        # Ten kod mówi: weź wszystkie pola z modelu i zamień je na JSON
-        fields = '__all__'
